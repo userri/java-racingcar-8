ed816d7903caf080c2c879a6fd8da3886e10f888
임유리 <dladbfl67@naver.com>
2025-10-27 23:44:59 +0900

    test : 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -30,6 +31,84 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    @DisplayName("연속된 구분자(예:,,)")
+    void test_consecutive_delimiters_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("구분자로 시작")
+    void test_starts_with_delimiter_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("구분자로 끝남")
+    void test_ends_with_delimiter_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("구분자 사이 빈 이름")
+    void test_empty_name_between_delimiters_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, ,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("이름 중복 입력")
+    void test_duplicate_names_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,pobi,woni", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+    }
+    @Test
+    @DisplayName("실행숫자에 음수 입력")
+    void test_negative_number_input_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    @DisplayName("실행숫자에 0 입력")
+    void test_zero_input_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+    }
+    @Test
+    @DisplayName("실행숫자에 문자 입력")
+    void test_character_input_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+    }
+    @Test
+    @DisplayName("실행숫자에 소수 입력")
+    void test_decimal_input_should_throw_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "1.0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+    }
+
 
     @Override
     public void runMain() {
